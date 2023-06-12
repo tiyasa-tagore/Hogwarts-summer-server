8e1c2985cdfb32aea77b94d53484429700f8272f
tiyasa-tagore <tiyasatagore1999@gmail.com>
2023-06-12 19:52:12 +0600

    mongo

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+
 
 // middleware
 app.use(cors());
